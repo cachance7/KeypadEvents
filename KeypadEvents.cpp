c6ae7e2c7d5d429623c58cc7752ad0730c0305c6
Casey Chance <caseyjchance@gmail.com>
2015-12-11 21:06:33 -0800

    Initial version

---
+++ KeypadEvents.cpp
@@ -0,0 +1,93 @@
+#include "KeypadEvents.h"
+
+#include <string.h>
+
+void noop(char) { }
+
+KeypadEvents::KeypadEvents() : col1(ANALOG_COUNTS(R1)),
+                               col2(ANALOG_COUNTS(R2)),
+                               col3(ANALOG_COUNTS(R3)),
+                               col4(ANALOG_COUNTS(R4))
+{
+    memset(last_states, -1, sizeof(int)*4);
+
+    /* Default handlers */
+    handlers[KEY_UP].function = noop;
+    handlers[KEY_DOWN].function = noop;
+    handlers[KEY_PRESSED].function = noop;
+}
+
+char (KeypadEvents::buttons[4][4]) = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'},
+};
+
+
+void KeypadEvents::onKeyDown(void (*function)(char)) {
+    handlers[KEY_DOWN].function = function;
+}
+
+void KeypadEvents::onKeyUp(void (*function)(char)) {
+    handlers[KEY_UP].function = function;
+}
+
+void KeypadEvents::onKeyPressed(void (*function)(char)) {
+    handlers[KEY_PRESSED].function = function;
+}
+
+void KeypadEvents::readEvents() {
+    rows[0] = analogRead(ROW1PIN);
+    rows[1] = analogRead(ROW2PIN);
+    rows[2] = analogRead(ROW3PIN);
+    rows[3] = analogRead(ROW4PIN);
+
+    int last, next;
+    for (int i = 0; i<4; i++) {
+        last = last_states[i];
+        next = getCol(rows[i]);
+
+        /* Possible outcomes:
+         *  1) last == next --> no change
+         *  2) last != next --> detect change
+         *     buttons[i][last] = UP
+         *     notifyUp(buttons[i][last])
+         *     notifyPressed(buttons[i][last])
+         *
+         *     buttons[i][next] = DOWN
+         *     notifyDown(buttons[i][next])
+         */
+
+        if (last != next) {
+            /* CAVEAT: Ignoring the Pressed -> Pressed transition (for now)
+             * e.g. last=2, next=3
+             */
+            if (next >= 0) {
+                /* Button was pressed */
+                handlers[KEY_DOWN].function(buttons[i][next]);
+            } else {
+                /* Button was released */
+                handlers[KEY_UP].function(buttons[i][last]);
+                handlers[KEY_PRESSED].function(buttons[i][last]);
+            }
+        }
+        last_states[i] = next;
+    }
+}
+
+int KeypadEvents::getCol(int reading) {
+    if (GET_RANGE(col1, reading)) {
+        return 0;
+    }
+    if (GET_RANGE(col2, reading)) {
+        return 1;
+    }
+    if (GET_RANGE(col3, reading)) {
+        return 2;
+    }
+    if (GET_RANGE(col4, reading)) {
+        return 3;
+    }
+    return -1;
+}
---
+++ KeypadEvents.h
@@ -0,0 +1,93 @@
+#ifndef KEYPAD_EVENTS_H
+#define KEYPAD_EVENTS_H
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#endif
+
+/* Event Codes */
+#define KEY_DOWN 0
+#define KEY_UP 1
+#define KEY_PRESSED 2
+
+/* Total number of event codes */
+#define EVENT_COUNT 3
+
+/* Key States */
+#define UP 0
+#define DOWN 1
+
+#ifndef ROW1PIN
+#define ROW1PIN 1
+#endif
+
+#ifndef ROW2PIN
+#define ROW2PIN 2
+#endif
+
+#ifndef ROW3PIN
+#define ROW3PIN 3
+#endif
+
+#ifndef ROW4PIN
+#define ROW4PIN 4
+#endif
+
+#ifndef R1
+#define R1 10000
+#endif
+
+#ifndef R2
+#define R2 22000
+#endif
+
+#ifndef R3
+#define R3 47000
+#endif
+
+#ifndef R4
+#define R4 68000
+#endif
+
+#ifndef RCOMM
+#define RCOMM 100000
+#endif
+
+#ifndef VCC
+#define VCC 5.0
+#endif
+
+/* For the default resistor values, this is a pretty good default. */
+#ifndef TOLERANCE
+#define TOLERANCE 0.004
+#endif
+
+#define ANALOG_COUNTS(r) ((VCC / 5.0) * 1024 * RCOMM / (RCOMM + r))
+#define PRINT_RANGE(col)({Serial.print(col - (col * TOLERANCE)); Serial.print(" < " #col " < "); Serial.print(col + (col * TOLERANCE)); Serial.println();})
+#define GET_RANGE(col,reading) ((col - (col * TOLERANCE)) < reading && reading < (col + (col * TOLERANCE)))
+
+class KeypadEvents {
+    public:
+        KeypadEvents();
+        void onKeyDown(void(*)(char));
+        void onKeyUp(void(*)(char));
+        void onKeyPressed(void(*)(char));  // Down and up
+        void readEvents();
+
+    private:
+        int getCol(int);
+        typedef struct _callback {
+			void (*function)(char);
+		} KeyEventCallback;
+        KeyEventCallback handlers[EVENT_COUNT];
+        const float col1;
+        const float col2;
+        const float col3;
+        const float col4;
+        int rows[4];
+        int col;
+
+        static char buttons[4][4];
+        int last_states[4]; // this is the last known count of each row
+};
+
+#endif
